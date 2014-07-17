5168e273519c9643d01f62763a3f3a905fca226e
shivadking <MacProgrammer1@Thiruvengadams-Mac-mini.local>
2014-07-17 13:11:36 +0530

    initial commit
    Sample Applications

---
+++ samp1/samp1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  samp1
+//
+//  Created by Thiruvengadam Krishnasamy on 17/07/14.
+//  Copyright (c) 2014 Thiruvengadam Krishnasamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
